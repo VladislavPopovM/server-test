b0173cbd78c27e859968530249d750a5963bd44c
Vladislav Popov <55542677+VladislavPopovM@users.noreply.github.com>
2024-01-11 14:41:21 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 from flask import Flask, request
+from dotenv import load_dotenv
 import requests
 import os
 
 app = Flask(__name__)
+load_dotenv()
 
 def get_external_version():
     """Sends a GET request to an external service and returns the response."""
-    external_url = os.getenv('EXTERNAL_SERVICE_URL')  # Get URL from environment variable
+    external_url = os.getenv("EXTERNAL_SERVICE_URL")  # Get URL from environment variable
     if not external_url:
         return "External service URL not configured"
 
@@ -16,9 +18,9 @@ def get_external_version():
     except requests.RequestException as e:
         return f"Error making request to external service: {e}"
 
-@app.route('/version', methods=['GET', 'POST'])
+@app.route("/version", methods=["GET", "POST"])
 def version():
-    if request.method == 'POST':
+    if request.method == "POST":
         # Processing incoming POST request
         data = request.json
         return "Data received: " + str(data)
@@ -27,5 +29,5 @@ def version():
         external_version = get_external_version()
         return "Response from external service: " + external_version
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5010)
